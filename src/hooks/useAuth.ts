02142ec9a975f0561b5a8cbea5d11dcc923c2fa6
JustinDosaj <justindosaj@gmail.com>
2025-06-18 17:43:01 -0700

    Update useAuth

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -4,7 +4,7 @@ import { AuthContext } from "@/providers/auth.providers";
 
 // Custom hook to use auth
 export const useAuth = () => {
-  const auth = useContext(AuthContext);
-  if (!auth) throw new Error("useAuth must be used within an AuthProvider");
-  return auth;
+    const auth = useContext(AuthContext);
+    if (!auth) throw new Error("useAuth must be used within an AuthProvider");
+    return auth;
 };
