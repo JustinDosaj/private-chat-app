0a986c8b524b7a114a30d217f20a813c0bd0a6f9
JustinDosaj <justindosaj@gmail.com>
2025-08-12 16:25:03 -0700

    Layout Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,8 @@
 import { Hero } from "@/components/ui/home/Hero";
 import { Navbar } from "@/components/elements/Navbar";
 import { Footer } from "@/components/elements/Footer";
-import { useAuth } from "@/hooks/useAuth";
 
 export default function Home() {
-
-  const { user } = useAuth();
-
-  console.log(user?.idToken)
 
   return (
     <>
--- src/components/ui/chat/Main.tsx
+++ src/components/ui/chat/Main.tsx
@@ -3,7 +3,6 @@ import React, { useEffect, useRef } from 'react';
 import MarkdownEditor from '@uiw/react-markdown-editor';
 import remarkGfm from 'remark-gfm';
 import Input from "./Input";
-import { SelectModel } from "./SelectModel";
 
 
 export default function Main({className, messages, handleSendMessage}: IMainView) {
@@ -19,9 +18,33 @@ export default function Main({className, messages, handleSendMessage}: IMainView
     return (
         <div className={`${className} h-screen overflow-hidden flex flex-col flex-1 bg-white`}>
             {/* Header */}
-            <SelectModel className="hidden md:flex border-b border-slate-300/40 p-4"/>
+    
 
             {/* Chat Messages Section */}
+            {/* <div ref={chatContainerRef} className="flex-1 overflow-y-auto p-6 space-y-4">
+                {messages.map((msg, index) => (
+                    <div key={index} className={`flex ${msg.sender === 'user' ? 'justify-end' : 'justify-start'}`}>
+                        <div className={`
+                            max-w-[100%] px-4 py-2 rounded-2xl
+                            ${msg.sender === 'user' 
+                            ? 'bg-blue-600 text-white' 
+                            : 'bg-white text-slate-900'}
+                        `}
+                        >
+                        {msg.sender === 'bot' ? (
+                            <MarkdownEditor.Markdown 
+                                remarkPlugins={[remarkGfm]}
+                                className="my-4"
+                                source={msg.content}
+                            />
+                        ) : (
+                            msg.content // Render plain text for user messages
+                        )}
+                        </div>
+                    </div>
+                ))}
+            </div> */}
+
             <div ref={chatContainerRef} className="flex-1 overflow-y-auto p-6 space-y-4">
                 {messages.map((msg, index) => (
                     <div key={index} className={`flex ${msg.sender === 'user' ? 'justify-end' : 'justify-start'}`}>
